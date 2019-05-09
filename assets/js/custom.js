7e5cc52f94a9e9c079a552d511fb596585ad3c3b
Elvia Gutierrez <elviagutierrez07@gmail.com>
2019-05-08 20:51:53 -0700

    added firebase js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,3 +2,51 @@
  * Custom JS
  * Use this file to add your custom scripts
  */
+
+// JavaScript Document
+
+var name;
+var phoneNumber;
+var email;
+//var dropLocation;
+var reserveDate;
+var reserveTime;
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDwmIb1yuSg3q-YtsmvxXh0IPkfkvZasig",
+    authDomain: "datacollect-4f475.firebaseapp.com",
+    databaseURL: "https://datacollect-4f475.firebaseio.com",
+    projectId: "datacollect-4f475",
+    storageBucket: "datacollect-4f475.appspot.com",
+    messagingSenderId: "370113740376",
+    appId: "1:370113740376:web:ac5ed59dba53b6ca"
+  };
+
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database()
+
+$(".submit").on("click",function(event){
+	
+event.preventDefault();
+	
+name = $("#reservation__form__name").val().trim();
+phoneNumber = $("#reservation__form__phone").val().trim();
+email = $("#reservation__form__email").val().trim();
+//dropLocation = $("#reservation__form__people").val();
+reserveDate = $("#reservation__form__date").val().trim();
+reserveTime = $("#reservation__form__time").val().trim();
+
+	database.ref().push({
+		name: name,
+		phoneNumber: phoneNumber,
+		email: email,
+		reserveDate : reserveDate,
+		reserveTime : reserveTime
+		
+		
+	})
+
+})
